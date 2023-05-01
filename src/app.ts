c2e080d3cc6b7310417f63722995029f8e22d077
Neil Alvarez Garcia <Neil8b.jjr2014@gmail.com>
2023-05-01 14:30:25 -0500

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
 
 import { schema } from "./schema/index";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+app.use(cors());
 
 app.use(
   "/graphql",
